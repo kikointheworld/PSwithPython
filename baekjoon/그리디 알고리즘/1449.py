76865453258a8680af74bf981b13f8696369d3c1
kkkiko <popo9855@g.skku.edu>
2022-01-05 22:39:28 +0900

    Upload 1449.py

---
+++ baekjoon/그리디 알고리즘/1449.py
@@ -0,0 +1,29 @@
+n, l = map(int, input().split())
+
+list1 = list(map(int, input().split()))
+
+list1.sort()
+
+ans = 0
+
+flag = 0
+
+target = 0
+
+for i in range(n):
+    if i == 0:
+        target = list1[i] + l - 1
+        ans += 1
+    
+    else:
+        if list1[i] <= target:
+            continue
+        else:
+            ans += 1
+            target = list1[i] + l - 1
+
+    
+
+
+
+print(ans)
